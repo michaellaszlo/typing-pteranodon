01edebf4d79f1394cdf47f8b9309cc10949d36c3
Michael Laszlo <git@michaellaszlo.com>
2015-05-09 16:56:36 -0400

    Improve input response

--- js/tp.js
+++ js/tp.js
@@ -1,8 +1,11 @@
 var TypingPteranodon = {
-  width: 600,
-  height: 600,
+  dimensions: {
+    chute: { width: 400, height: 500 },
+    typing: { width: 300, height: 50 }
+  },
   hertz: 60,
-  dictionary: dictionary17870
+  dictionary: dictionary17870,
+  update: {}
 };
 
 TypingPteranodon.makeLevel = function () {
@@ -22,15 +25,15 @@ TypingPteranodon.makeLevel = function () {
 TypingPteranodon.nextWord = function () {
   var g = TypingPteranodon,
       level = g.level,
-      context = g.canvas.context,
+      context = g.context,
       word = g.word,
       wordIndex = ++g.wordIndex;
   if (wordIndex == level.numWords) {
     g.stop();
   }
   word.text = level.words[wordIndex]; 
-  word.width = context.measureText(word.text).width;
-  word.x = (g.width - word.width) / 2;
+  word.width = context.chute.measureText(word.text).width;
+  word.x = (g.dimensions.chute.width - word.width) / 2;
   word.y = g.startY;
 };
 
@@ -42,62 +45,85 @@ TypingPteranodon.play = function () {
   g.wordIndex = -1;
   g.nextWord();
   g.ticks = 0;
-  g.updateInterval = window.setInterval(g.update, 1000/g.hertz);
+  g.interval = { chute: window.setInterval(g.update.chute, 1000/g.hertz) };
 }
 
 TypingPteranodon.stop = function () {
   var g = TypingPteranodon;
-  window.clearTimeout(g.updateInterval);
-}
+  window.clearTimeout(g.interval.chute);
+};
 
-TypingPteranodon.update = function () {
+TypingPteranodon.update.chute = function () {
   var g = TypingPteranodon,
-      context = g.canvas.context,
-      word = g.word;
-  context.clearRect(0, 0, g.width, g.height);
-  context.fillText(word.text, word.x, word.y);
+      word = g.word,
+      chuteCanvas = g.canvas.chute,
+      chuteContext = g.context.chute;
+  g.ticks += 1;
+  g.stopwatch.innerHTML = Math.floor(g.ticks/g.hertz);
+  chuteContext.clearRect(0, 0, chuteCanvas.width, chuteCanvas.height);
+  chuteContext.fillText(word.text, word.x, word.y);
   word.y += word.speed;
   if (word.y >= g.finishY) {
     g.nextWord();
   }
-  g.ticks += 1;
-  g.stopwatch.innerHTML = Math.floor(g.ticks/g.hertz);
-  var attempt = g.typing.input.value;
-  g.typing.display.innerHTML = attempt;
-  for (var i = 0; i < word.length && i < attempt.length; ++i) {
-    var c = word.charAt(i),
+};
+
+TypingPteranodon.update.typing = function () {
+  var g = TypingPteranodon,
+      word = g.word,
+      attempt = g.input.value,
+      typingCanvas = g.canvas.typing,
+      typingContext = g.context.typing;
+  typingContext.clearRect(0, 0, typingCanvas.width, typingCanvas.height),
+  typingContext.fillText(attempt, 10, 35);
+  return;
+  for (var i = 0; i < word.text.length && i < attempt.length; ++i) {
+    var c = word.text.charAt(i),
         d = attempt.charAt(i);
   }
-}
+};
 
-TypingPteranodon.focus = function () {
-  var g = TypingPteranodon,
-      typing = g.typing;
+TypingPteranodon.make = function (tag, options) {
+  var element = document.createElement(tag);
+  if (options !== undefined) {
+    if (options.id !== undefined) {
+      element.id = options.id;
+    }
+    if (options.class !== undefined) {
+      element.className = options.class;
+    }
+    if (options.in !== undefined) {
+      options.in.appendChild(element);
+    }
+  }
+  return element;
 };
 
 TypingPteranodon.load = function () {
   var g = TypingPteranodon,
-      canvas = g.canvas = document.getElementById('mainCanvas');
-  canvas.width = g.width;
-  canvas.height = g.height;
-  var context = canvas.context = canvas.getContext('2d');
-  context.font = '30px sans-serif';
+      wrapper = document.getElementById('wrapper'),
+      container = g.make('div', { id: 'gameContainer', in: wrapper }),
+      canvas = g.canvas = {
+        chute: g.make('canvas', { id: 'chute', in: container }),
+        typing: g.make('canvas', { id: 'typingDisplay', in: wrapper })
+      },
+      context = g.context = {
+        chute: canvas.chute.getContext('2d'),
+        typing: canvas.typing.getContext('2d')
+      },
+      input = g.input = g.make('input', { id: 'typingInput', in: wrapper }),
+      stopwatch = g.stopwatch = g.make('div', { id: 'stopwatch', in: wrapper });
+  canvas.chute.width = g.dimensions.chute.width;
+  canvas.chute.height = g.dimensions.chute.height;
+  canvas.typing.width = g.dimensions.typing.width;
+  canvas.typing.height = g.dimensions.typing.height;
+  context.chute.font = context.typing.font = '30px sans-serif';
   g.startY = -15;
   g.finishY = 645;
-  g.stopwatch = document.getElementById('stopwatch');
 
-  var typing = g.typing = {
-    input: document.getElementById('typingInput'),
-    display: document.getElementById('typingDisplay')
-  };
-
-  typing.input.focus();
-  typing.input.onblur = function () {
-    typing.input.focus();
-  };
-  typing.input.onfocus = function () {
-    g.focus();
-  };
+  input.focus();
+  input.onblur = input.focus;
+  input.oninput = g.update.typing;
 
   g.play();
 };
