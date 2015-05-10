9c4a01a24aecfbf78fee740943c285bf550a70fc
Michael Laszlo <git@michaellaszlo.com>
2015-05-09 20:33:48 -0400

    Measure height of text on canvas

--- js/tp.js
+++ js/tp.js
@@ -1,7 +1,11 @@
 var TypingPteranodon = {
-  dimensions: {
-    chute: { width: 400, height: 500 },
+  layout: {
+    chute: { width: 400, height: 500, left: 50, top: 50 },
     typing: { width: 300, height: 50 }
+  },
+  font: {
+    face: 'sans-serif',
+    size: { pixels: 30 },
   },
   hertz: 60,
   dictionary: dictionary17870,
@@ -25,6 +29,7 @@ TypingPteranodon.makeLevel = function () {
 TypingPteranodon.nextWord = function () {
   var g = TypingPteranodon,
       level = g.level,
+      canvas = g.canvas,
       context = g.context,
       word = g.word,
       wordIndex = ++g.wordIndex;
@@ -32,9 +37,37 @@ TypingPteranodon.nextWord = function () {
     g.stop();
   }
   word.text = level.words[wordIndex]; 
-  word.width = context.chute.measureText(word.text).width;
-  word.x = (g.dimensions.chute.width - word.width) / 2;
+  word.width = Math.ceil(context.chute.measureText(word.text).width);
+  word.x = (g.layout.chute.width - word.width) / 2;
   word.y = g.startY;
+
+  // Measure the height of the word.
+  context.stage.clearRect(0, 0, canvas.stage.width, canvas.stage.height);
+  context.stage.fillText(word.text, 0, g.font.base.top);
+  var testWidth = word.width,
+      testHeight = canvas.stage.height,
+      testData = context.stage.getImageData(0, 0, testWidth, testHeight).data;
+  for (var i = 3; ; i += 4) {  // Seek forward for a non-transparent pixel.
+    if (testData[i] != 0) {
+      var x = (i-3)/4,
+          c = x % testWidth,
+          firstRow = (x-c) / testWidth;
+      break;
+    }
+  }
+  for (var i = 4*testWidth*testHeight - 1; ; i -= 4) {  // Seek backward.
+    if (testData[i] != 0) {
+      var x = (i-3)/4,
+          c = x % testWidth,
+          lastRow = (x-c) / testWidth;
+      break;
+    }
+  }
+  word.height = lastRow-firstRow+1;
+  context.stage.fillStyle = '#edb0b0';
+  context.stage.fillRect(0, firstRow, word.width, word.height);
+  context.stage.fillStyle = 'black';
+  context.stage.fillText(word.text, 0, g.font.base.top);
 };
 
 TypingPteranodon.play = function () {
@@ -75,7 +108,7 @@ TypingPteranodon.update.typing = function () {
       typingCanvas = g.canvas.typing,
       typingContext = g.context.typing;
   typingContext.clearRect(0, 0, typingCanvas.width, typingCanvas.height),
-  typingContext.fillText(attempt, 10, 35);
+  typingContext.fillText(attempt, g.font.base.left, g.font.base.top);
   return;
   for (var i = 0; i < word.text.length && i < attempt.length; ++i) {
     var c = word.text.charAt(i),
@@ -104,27 +137,46 @@ TypingPteranodon.load = function () {
       wrapper = document.getElementById('wrapper'),
       container = g.make('div', { id: 'gameContainer', in: wrapper }),
       canvas = g.canvas = {
+        stage: g.make('canvas', { in: wrapper }),
         chute: g.make('canvas', { id: 'chute', in: container }),
         typing: g.make('canvas', { id: 'typingDisplay', in: wrapper })
       },
       context = g.context = {
+        stage: canvas.stage.getContext('2d'),
         chute: canvas.chute.getContext('2d'),
         typing: canvas.typing.getContext('2d')
       },
       input = g.input = g.make('input', { id: 'typingInput', in: wrapper }),
-      stopwatch = g.stopwatch = g.make('div', { id: 'stopwatch', in: wrapper });
-  canvas.chute.width = g.dimensions.chute.width;
-  canvas.chute.height = g.dimensions.chute.height;
-  canvas.typing.width = g.dimensions.typing.width;
-  canvas.typing.height = g.dimensions.typing.height;
-  context.chute.font = context.typing.font = '30px sans-serif';
-  g.startY = -15;
-  g.finishY = 645;
+      stopwatch = g.stopwatch = g.make('div', { id: 'stopwatch', in: wrapper }),
+      layout = g.layout;
+  canvas.chute.width = g.layout.chute.width;
+  canvas.chute.height = g.layout.chute.height;
+  canvas.stage.width = canvas.typing.width = g.layout.chute.width;
+  canvas.stage.height = canvas.typing.height = 2 * g.font.size.pixels;
+  input.style.left = container.style.left = canvas.typing.style.left =
+      layout.chute.left + 'px';
+  canvas.chute.style.top = layout.chute.top + 'px';
+  input.style.top = canvas.typing.style.top =
+      layout.chute.top + layout.chute.height + 'px';
+  canvas.stage.style.position = 'fixed';
+  canvas.stage.style.top = layout.chute.top + 'px';
+  canvas.stage.style.left = layout.chute.left + layout.chute.width + 5 + 'px';
+  canvas.stage.style.border = '1px dotted #ddd';
+
+  var font = g.font.size.pixels + 'px ' + g.font.face;
+  context.stage.font = context.chute.font = context.typing.font = font;
+  g.font.base = {
+    left: Math.floor(g.font.size.pixels/2),
+    top: g.font.size.pixels +
+      Math.floor((g.layout.typing.height - g.font.size.pixels)/2)
+  };
 
   input.focus();
   input.onblur = input.focus;
   input.oninput = g.update.typing;
 
+  g.startY = 0;
+  g.finishY = layout.chute.height;
   g.play();
 };
 
